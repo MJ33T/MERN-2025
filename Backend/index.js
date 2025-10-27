064512e3a1776cef40aa30539f4bdca00f697582
Md. Muhtasim Jeet <57042167+MJ33T@users.noreply.github.com>
2025-10-27 13:06:30 +0600

    connected with db and create add api

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,26 @@
 import express from "express";
+import { collectionName, connection } from "./dbconfig.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Hello from Express!",
-    success: true,
-  });
+app.use(express.json());
+
+app.post("/add-task", async (req, res) => {
+  const db = await connection();
+  const collection = db.collection(collectionName);
+
+  const result = await collection.insertOne(req.body);
+
+  if (result) {
+    res.send({
+      message: "Task added successfully",
+      success: true,
+      data: result,
+    });
+  } else {
+    res.send({ message: "Failed to add task", success: false, data: null });
+  }
 });
+
 
 app.listen(3000);
